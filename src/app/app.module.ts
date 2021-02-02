c26ca70f7608a6d0828e891d893aa238a3e63de9
KevinKhau <kkhau@hotmail.fr>
2021-02-02 20:45:02 +0100

    Title + Routing update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -37,9 +37,6 @@ export const material = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([
-      {path: 'poker', component: PokerComponent},
-    ]),
     ...material,
   ],
   providers: [],
